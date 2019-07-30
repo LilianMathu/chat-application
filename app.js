e6dac1a82a616af8f76ff14bce4bb00c2bd651a3
LilianMathu <lilianmathu2018@gmail.com>
2019-07-30 11:57:15 +0300

    send and receive messages

--- app.js
+++ app.js
@@ -13,13 +13,22 @@ app.use(express.static(path.join(__dirname, '/public')));
 //This is the server side connection The client side connection will be set up in the index.html file
 // open connections using the io variable declared above
 // on is a method availed by socket.io
-//connection is an event
+// connection is an event
 // socket argument will be used each time we want to send and receive messages
+// emit and on methods are the primary methods for handling events in socket.io for both client and server
+// message-from-server is a string event listener
 
 io.on('connection', function(socket){
     console.log('A new connection has been made');
-}) 
 
+    socket.emit('message-from-server', {
+        greeting: "Hello from server"
+    });
+
+    socket.on('message-from-client', function(msg){
+        console.log(msg);
+    })
+}) 
 
 server.listen(port, ()=> {
     console.log(`server is up and running on port ${port}`);
